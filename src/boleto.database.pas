f820c212179fcef003ce559756fc074df3a5e7fe
Silas Santos <sillassanttos@outlook.com>
2023-03-11 15:41:41 -0400

    Adicionado conexao com banco de dados via arquivo ini

--- src/boleto.database.pas
+++ src/boleto.database.pas
@@ -7,6 +7,7 @@ interface
 uses
   Classes,
   SysUtils,
+  IniFiles,
   ZConnection;
 
 type
@@ -15,6 +16,8 @@ type
 
   TDM = class(TDataModule)
     Conexao: TZConnection;
+    procedure ConexaoBeforeConnect(Sender: TObject);
+    procedure DataModuleCreate(Sender: TObject);
   private
 
   public
@@ -28,5 +31,29 @@ implementation
 
 {$R *.lfm}
 
+{ TDM }
+
+procedure TDM.DataModuleCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TDM.ConexaoBeforeConnect(Sender: TObject);
+begin
+  with TIniFile.Create(ChangeFileExt('boleto', '.ini')) do
+  try
+    Conexao.HostName := ReadString('Database', 'IP', '');
+    Conexao.Database := ReadString('Database', 'BancoDados', '');
+    Conexao.Protocol := ReadString('Database', 'Protocolo', '');
+    Conexao.User := ReadString('Database', 'Usuario', '');
+    Conexao.Password := ReadString('Database', 'Senha', '');
+    Conexao.Port := ReadInt64('Database', 'PortaDB', 3306);
+    Conexao.LibraryLocation := ReadString('Database', 'LibraryLocation', '');
+  finally
+    Free;
+  end;
+end;
+
 end.
+
 
